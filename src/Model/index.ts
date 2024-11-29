d7a848114b5c00069b79ba6ee13765b93d287625
Erik Slovak <erik.slovak@technologystudio.sk>
2024-11-29 20:58:59 +0100

    feat: add `VALIDATION_ERROR`

---
+++ src/Model/index.ts
@@ -0,0 +1,7 @@
+/**
+ * @Author: Erik Slovak <erik.slovak@technologystudio.sk>
+ * @Date: 2024-11-29T20:58:28+01:00
+ * @Copyright: Technology Studio
+**/
+
+export const VALIDATION_ERROR = 'VALIDATION_ERROR'
